3716e0377b1b9acd8add27e6675502de19bf3aed
Rasmus Lindved <ralin@eg.dk>
2020-07-06 19:27:18 +0200

    Able to calculate score for Sun calls

--- src/app/whist/services/whist-calculator.service.ts
+++ src/app/whist/services/whist-calculator.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Call, CallType} from '../model/Call';
 import {Player} from '../../shared/model/Player';
-import {TRICK_TABLES} from '../model/TrickTable';
+import {TRICK_TABLE_SUN, TRICK_TABLES} from '../model/TrickTable';
 import {ScoreService} from '../../shared/services/score.service';
 
 export interface CalculateWhistInformation {
@@ -20,22 +20,40 @@ export class WhistCalculatorService {
     }
 
     public calculateScore(calculateInformation: CalculateWhistInformation, players: Player[]): void {
+        if (calculateInformation.call.majorType === CallType.REGULAR) {
+            this.calculateRegularScore(calculateInformation, players);
+        } else {
+            this.calculateSunScore(calculateInformation, players);
+        }
+    }
+
+    private calculateRegularScore(calculateInformation: CalculateWhistInformation, players: Player[]): void {
         const trickTable: Map<CallType, Map<number, number>> = TRICK_TABLES.get(+calculateInformation.targetSticks);
         const trickTableElement: Map<number, number> = trickTable.get(calculateInformation.call.callType);
         const difference = calculateInformation.acquiredSticks - calculateInformation.targetSticks;
-        const pointToBeGiven = trickTableElement.get(difference);
+        const pointsToBeGiven = trickTableElement.get(difference);
         const isSelfPartner = calculateInformation.partner.id === -1;
         if (isSelfPartner) {
-
+            this.scoreService.addScore(calculateInformation.caller, pointsToBeGiven * 3);
+            const opposition = players.filter(player => player.id !== calculateInformation.caller.id);
+            opposition.forEach(opponent => this.scoreService.addScore(opponent, pointsToBeGiven * -1));
         } else {
             const opposition = players.filter(player =>
                 player.id !== calculateInformation.caller.id
                 && player.id !== calculateInformation.partner.id
             );
-            this.scoreService.addScore(calculateInformation.caller, pointToBeGiven);
-            this.scoreService.addScore(calculateInformation.partner, pointToBeGiven);
-            this.scoreService.addScore(opposition[0], pointToBeGiven * -1);
-            this.scoreService.addScore(opposition[1], pointToBeGiven * -1);
+            this.scoreService.addScore(calculateInformation.caller, pointsToBeGiven);
+            this.scoreService.addScore(calculateInformation.partner, pointsToBeGiven);
+            this.scoreService.addScore(opposition[0], pointsToBeGiven * -1);
+            this.scoreService.addScore(opposition[1], pointsToBeGiven * -1);
         }
+    }
+
+    private calculateSunScore(calculateInformation: CalculateWhistInformation, players: Player[]): void {
+        const pointsToBeGiven = TRICK_TABLE_SUN.get(calculateInformation.call.callType);
+        const invertPoints: number = calculateInformation.sunWinningCondition ? 1 : -1;
+        this.scoreService.addScore(calculateInformation.caller, pointsToBeGiven * invertPoints);
+        const opposition = players.filter(player => player.id !== calculateInformation.caller.id);
+        opposition.forEach(opponent => this.scoreService.addScore(opponent, (pointsToBeGiven / 3) * (-1 * invertPoints)));
     }
 }
